9a09361cdf1595a9e80a005f89fe29d9b2cda635
Sushil <SushilMishra0712>
2019-11-20 11:44:31 +0530

    values readed from dictionary into an array

--- Prog_Constructs.php
+++ Prog_Constructs.php
@@ -23,7 +23,17 @@ echo "Result of ".$c."+(".$a."/".$b.") is:".$result3."\n";
 $result4 = ($a%$b)+$c;
 echo "Result of (".$a."%".$b.")+".$c." is:".$result4."\n";
 
+//arrays to store the results in dictionary
 $dictionary = array("UseCase-2"=>"$result1","UseCase-3"=>"$result2","UseCase-4"=>$result3,"UseCase-5"=>$result4);
+$size_of_dictionary = count($dictionary);
 print_r($dictionary);
+
+//array to read the values from the dictionary
+$read_array = array();
+for($i=2;$i<$size_of_dictionary+2;$i++)
+{
+    array_push($read_array,$dictionary["UseCase-".$i]);
+}
+print_r($read_array);
 
 ?>
